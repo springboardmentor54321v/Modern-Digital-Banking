ec71d60ff14dbb9cd6666404afc034f7739b7830
Sridhar <sridharrm2006@gmail.com>
2026-02-15 08:03:30 +0530

    Delete Backend/app directory

--- Backend/app/main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from routes.accounts import account
-app = FastAPI()
-app.include_router(account)
-# -----test------
-@app.get("/test")
-def test():
-    return{"message":"fast Api"}
--- Backend/app/routes/accounts.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-
-account = APIRouter()
-# ----------------------test--------------
-@account.get("/accounts")
-def account_check():
-    return{"message":"Router Works"}
