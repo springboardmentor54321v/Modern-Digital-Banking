fa30e6ec75ea62989f5a067e72c97fd299e53d54
Sridhar Mani <sridharrm2006@outlook.com>
2026-02-15 08:15:19 +0530

    Login successufully works

--- Backend/app/routes/users.py
+++ Backend/app/routes/users.py
@@ -2,7 +2,8 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from app.database import get_db
 from app.schemas.user import UserCreate
-from app.services.auth_service import new_user
+from app.schemas.auth import LoginUser
+from app.services.auth_service import new_user,login
 
 router = APIRouter(prefix="/users", tags=["Users"])
 
@@ -14,3 +15,8 @@ def add_user(user: UserCreate, db: Session = Depends(get_db)):
 # @router.get("/", response_model=list[UserResponse])
 # def read_users(db: Session = Depends(get_db)):
 #     return get_users(db)
+
+@router.post("/login")
+def login_user(user:LoginUser,db:Session = Depends(get_db)):
+    return login(db,user)
+
