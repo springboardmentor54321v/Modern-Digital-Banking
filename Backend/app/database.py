4e0d0e771a8a9b2c618ceb71834cc260537a4d2d
Sridhar Mani <sridharrm2006@outlook.com>
2026-02-15 09:23:36 +0530

    changes

--- Backend/app/database.py
+++ Backend/app/database.py
@@ -1,9 +1,14 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
+# from dotenv import load_dotenv        
+
+
+
+
 
 DATABASE_URL = "postgresql://postgres:##Sridhar2006@localhost:5432/Digital_Bank"
-
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
