993f962e4695582223096eee6c6fa2c93fc61059
ss2025-arch <sandhiyasureshkannan@gmail.com>
2026-04-02 19:11:28 +0530

    Delete no duplicate alerts.sql

--- no duplicate alerts.sql
+++
@@ -1,6 +0,0 @@
-/*ALTER TABLE alerts
-DROP CONSTRAINT unique_daily_alert;*/
-INSERT INTO alerts (user_id, type, created_at, message)
-VALUES (1, 'budget_exceeded', NOW(), 'Budget exceeded');
-INSERT INTO alerts (user_id, type, created_at, message)
-VALUES (1, 'budget_exceeded', NOW(), 'Budget exceeded');
