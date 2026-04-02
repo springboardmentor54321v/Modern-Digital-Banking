7c445d52c7810afaee2a714503b0778898a1f1b8
ss2025-arch <sandhiyasureshkannan@gmail.com>
2026-04-02 19:03:28 +0530

    Delete admin logs table.sql

--- admin logs table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS admin_logs (
-    id SERIAL PRIMARY KEY,
-    admin_id INT REFERENCES users(id),
-    action TEXT,
-    target_type VARCHAR(50),
-    target_id INT,
-    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-SELECT*FROM admin_logs;
