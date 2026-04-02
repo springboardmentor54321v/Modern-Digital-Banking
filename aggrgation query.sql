27adf6e19376c57e80f6227b2cdb1d90fefa2960
ss2025-arch <sandhiyasureshkannan@gmail.com>
2026-04-02 19:03:47 +0530

    Delete aggrgation query.sql

--- aggrgation query.sql
+++
@@ -1,7 +0,0 @@
-EXPLAIN ANALYZE
-SELECT category,
-       SUM(amount) AS total_amount
-FROM transactions
-WHERE account_id = 1
-AND txn_date BETWEEN '2025-01-01' AND '2025-01-31'
-GROUP BY category;
